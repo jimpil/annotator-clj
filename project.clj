7984daf463a14ff4963d8374fb673654b700ae36
jimpil <jimpil1985@gmail.com>
2013-01-07 16:30:28 +0000

    first commit

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject PAnnotator "0.2.0"
-  :description "A parallel, dictionary-based annotator for NLP-related tasks."
+  :description "A parallel, dictionary-based annotator for Text-mining & NLP-related tasks."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
