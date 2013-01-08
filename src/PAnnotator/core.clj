3207226fdedce5cfaa6fefe8b7262ac1432fe166
Dimitrios Piliouras <jimpil1985@gmail.com>
2013-01-08 12:16:41 +0000

    more flexible API. Can now accomodate other tagging schemes.

--- src/PAnnotator/core.clj
+++ src/PAnnotator/core.clj
@@ -1,5 +1,6 @@
 (ns PAnnotator.core
-  (:use [clojure.tools.cli :only [cli]]))
+  (:use [clojure.tools.cli :only [cli]]
+        [clojure.set :only [union]]))
 
 
 (defn string->data
@@ -8,6 +9,16 @@
 [f]
  (binding [*read-eval* false]
  (read-string (slurp f))))
+ 
+ 
+(defn- combine-dicts 
+"Takes a seq of dictionaries (seqs) and produces a united set of all entries (no duplicates)."
+ [ds]
+ (if (< 1 (count ds))
+   (let [set-views (map set ds)]
+     (apply union set-views)) (first ds))) 
+    
+ ;;(mapv combine-dicts dicts)
  
 (defn- space-out 
 "Given some text, find all the sgml tags that are not surrounded by spaces and put spaces around them." 
@@ -18,10 +29,10 @@
 (.replaceAll " $0"))) 
 
 (defn-  un-capitalize ^String [^String s]
-(if-not (every? #(Character/isUpperCase %) s) 
+(if (every? #(Character/isUpperCase %) s) s 
   (let [Cfirst (subs s 0 1)
         Crest  (subs s 1) ]
-  (str (.toLowerCase Cfirst) Crest)) s))
+  (str (.toLowerCase Cfirst) Crest))))
 
 (defn- annotate 
  "Overloaded function. 
@@ -36,26 +47,27 @@
     (annotate {:entity-type  \"drug\" :target \"test.txt\"  ;;resulting sgml: <START:drug> ... <END>
                :file+dics [[\"train/invitro_train-raw.txt\" \"train/invitro-train-names-distinct.txt\"] 
                            [\"train/invivo_train-raw.txt\"  \"train/invivo-train-names-distinct.txt\"]]})"
-(^String [^String f ^String entity-type dictionaries]
-  (reduce     
-         (fn [file dic]  
-           (println (str "Processing document:" file)) 
-           (loop [text  (slurp file)
-                  names (map #(.trim %)  ;drugbank-list
-                              (clojure.string/split-lines (slurp dic)))]
+(^String [^String f ^String entity-type dics ^String op-tag ^String mi-tag ^String cl-tag]
+  (let [dic (combine-dicts (mapv (comp (fn [untrimmed] (mapv #(.trim ^String %) untrimmed)) 
+                                      clojure.string/split-lines 
+                                      slurp) dics))]    
+         ;(fn [file dic]  
+           (println (str "Queueing document: " f)) 
+           (loop [text  (slurp f)
+                  names dic ]
   	     (if-let [name1 (first names)] 
            (recur (try ;(do (println (str "ANNOTATING " name1)) ;then clause
               (.replaceAll 
         	(re-matcher 
                 (re-pattern (str "(?i)\\b+" (java.util.regex.Pattern/quote name1) "+\\b")) text)  
-             (str "<START:" entity-type "> " (un-capitalize name1) " <END>")) 
+             (str op-tag entity-type mi-tag (un-capitalize name1) cl-tag)) 
              (catch java.util.regex.PatternSyntaxException _ 
              (do (println (str "--- CANNOT BE PROCESSED! --->" name1)) text)))
-             (rest names)) text))) 
-   f dictionaries))  
-([{:keys [files+dics entity-type target]
-   :or {entity-type "default"}}]
- (let [annotations (pmap #(space-out (annotate (first %) entity-type  (rest %))) files+dics)] ;will return a list of (annotated) strings
+             (rest names)) text)) 
+   )) 
+([{:keys [files+dics entity-type target op-tag cl-tag mi-tag]
+   :or {entity-type "default" target "target-file.txt" op-tag "<START:" mi-tag "> "  cl-tag " <END>"}}]
+ (let [annotations (pmap #(space-out (annotate (first %) entity-type  (rest %) op-tag mi-tag cl-tag)) files+dics)] ;will return a list of (annotated) strings
   (doseq [a annotations]
    (spit target a :append true)
    (spit target "\n" :append true)))) );;append new line per document (for the adaptive feature generator)
@@ -65,33 +77,22 @@
  (gen-class :name Annotator
              :main true
              :methods [^:static [process [java.util.Map] void]
-                       ^:static [process [String String java.util.List] String]]) 
-
-#_(defn -main [& [d t e]]
- (when-not (and d t)
-  (println 
-"\nINSTRUCTIONS: java -cp annotator-uberjar.jar Annotator [DATA-FILE]* [TARGET-FILE] [ENTITY-TYPE]** \n
-*must be a file with a 2d clojure seq of the form: [[input1 dic1 dic2 dic3 dic4] 
-                                                    [input2 dic5 dic6 dic7] 
-                                                    [input3 dic8 dic9]]
-**optional argument - defaults to \"default\"") (System/exit 1))
-(-process {:entity-type  e  :target t 
-           :files+dics (string->data d)}) (shutdown-agents))  
+                       ^:static [process [String String java.util.List] String]])  
 
 
-(def HELP_MESSAGE "\nINSTRUCTIONS: java -cp PAnnotator-uberjar.jar Annotator -d <DATA-FILE>* -t <TARGET-FILE>** -e <ENTITY-TYPE>*** \n
+(def HELP_MESSAGE "\nINSTRUCTIONS: java -cp PAnnotator-uberjar.jar Annotator -d <DATA-FILE>* 
+                                                                             -t <TARGET-FILE>** 
+                                                                             -e <ENTITY-TYPE>*** 
+                                                                             -o <OPENING-TAG>****
+                                                                             -c <CLOSING-TAG>****
+                                                                             -m <END OF OPENING-TAG>**** \n
 *must be a file with a 2D clojure seqable of the form: [[input1 dic1 dic2 dic3 dic4] 
                                                         [input2 dic5 dic6 dic7] 
                                                         [input3 dic8 dic9]]
 *defaults to 'data-file.txt' 
 **defaults to 'target-file.txt'
-***optional argument - defaults to \"default\"")
-
-(defn- run
-  "Print out the options and the complementing arguments."
-  [opts args]
-  (println (str "Options:\n" opts "\n\n"))
-  (println (str "Arguments:\n" args "\n\n")))
+***optional argument - defaults to \"default\"
+****optional arguments - they all default to the standard openNLP annotation tags.")
 
 (defn -main [& args]
   (let [[opts argus banner]
@@ -100,13 +101,21 @@
              ["-d" "--data" "REQUIRED: The data-file (e.g. 'data.txt')" :default "data-file.txt"]
              ["-e" "--entity-type" "REQUIRED: The type of entity in question (e.g. 'river')" :default "default"]
              ["-t" "--target" "REQUIRED: The target-file (e.g. 'target.txt')" :default "target-file.txt"] 
-             ;["-gdic" "--global-dictionary" "OPTIONAL: Specify a global dictionary to be used for all annotations."]
+             ["-o" "--op-tag" "REQUIRED: Specify the opening tag." :default "<START:"]
+             ["-m" "--mi-tag" "REQUIRED: Specify the end of the opening tag." :default "> "]
+             ["-c" "--cl-tag" "REQUIRED: Specify the closing tag."  :default " <END>"]
              )]
     (when (:help opts)
       (println HELP_MESSAGE "\n\n" banner)
       (System/exit 0))
   (do (-process {:entity-type  (:entity-type opts)  
                  :target       (:target opts)
-                 :files+dics   (string->data (:data opts))}) 
+                 :files+dics   (string->data (:data opts))
+                 :op-tag       (:op-tag opts)
+                 :mi-tag       (:mi-tag opts)
+                 :cl-tag       (:cl-tag opts)})
+    (println "--------------------------------------------------------\n"
+             "SUCCESS! Look for a file called" (str "'" (:target opts) "'.\n"))              
     (shutdown-agents) 
     (System/exit 0)) ))
+    
