c8db757ecee48c43912e13b12c7d7b5fde77fb2f
Dimitrios Piliouras <jimpil1985@gmail.com>
2013-01-12 02:09:57 +0000

    some JVM optimisations that i had forgotten

--- project.clj
+++ project.clj
@@ -5,10 +5,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0-RC1"]
                  [org.clojure/tools.cli "0.2.2"]]
-  :jvm-opts ["-Xmx1g" "-server" "-XX:+UseCompressedOops"]
+  :jvm-opts ["-Xmx1g" "-server" 
+             "-XX:+OptimizeStringConcat" 
+             "-XX:+UseCompressedOops" 
+             ;"-XX:+UseCompressedStrings"
+             "-XX:+UseStringCache"]
   :jar-name "PAnnotator.jar"          ; name of the jar produced by 'lein jar'
   :uberjar-name "PAnnotator-uber.jar" ; same for 'lein uberjar'
-  :java-source-paths ["src/java"]
+  ;:java-source-paths ["src/java"]
   ;:aot []
   :main PAnnotator.core
   )
