4c980559e288f7bacf0a02b3debca54da8a7f06b
Dimitrios Piliouras <jimpil1985@gmail.com>
2013-01-16 00:58:57 +0000

    bumped version to 0.2.9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject PAnnotator "0.2.7"
+(defproject PAnnotator "0.2.9"
   :description "A parallel, dictionary-based annotator for Text-mining & NLP-related tasks."
   :url "https://github.com/jimpil/annotator-clj"
   :license {:name "Eclipse Public License"
