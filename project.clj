a6fed904a6c7a6c985fe45662cb7d8c6bbcfda21
Dimitrios Piliouras <jimpil1985@gmail.com>
2013-01-20 15:30:06 +0000

    bumped to version 0.3.2

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject PAnnotator "0.3.1"
+(defproject PAnnotator "0.3.2"
   :description "A parallel, dictionary-based annotator for Text-mining & NLP-related tasks."
   :url "https://github.com/jimpil/annotator-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0-RC1"]
-                 [org.clojure/tools.cli "0.2.2"]]
+                 [org.clojure/tools.cli "0.2.2"]
+                 [org.apache.lucene/lucene-snowball "3.0.3"]]
   :jvm-opts ["-Xmx1g" "-server" 
              "-XX:+OptimizeStringConcat" 
              ;"-XX:+UseCompressedOops" 
@@ -14,5 +15,9 @@
   :uberjar-name "PAnnotator-uber.jar" ; same for 'lein uberjar'
   ;:java-source-paths ["src/java"]
   ;:aot []
+  ;:warn-on-reflection true
   :main PAnnotator.core
+  :pom-addition [:developers  [:developer {:id "jimpil"}
+                              [:name "Dimitrios Piliouras"]
+                              [:url "http://www.cs.man.ac.uk/~piliourd/"]]]
   )
